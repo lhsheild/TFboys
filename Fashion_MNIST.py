5e1a4b02e6a0386ec5d8537263305cf8ee09a7a7
sheildog <lhsheild@yahoo.com>
2019-07-03 11:32:38 +0800

    图像分类

--- Fashion_MNIST.py
+++ Fashion_MNIST.py
@@ -10,8 +10,45 @@ def normalize(images, labels):
     return images, labels
 
 
+def plot_image(i, predictions_array, true_labels, images):
+    predictions_array, true_label, img = predictions_array[i], true_labels[i], images[i]
+    plt.grid(False)
+    plt.xticks([])
+    plt.yticks([])
+
+    plt.imshow(img[..., 0], cmap=plt.cm.binary)
+
+    predicted_label = np.argmax(predictions_array)
+    if predicted_label == true_label:
+        color = 'blue'
+    else:
+        color = 'red'
+
+    plt.xlabel("{} {:2.0f}% ({})".format(class_names[predicted_label],
+                                         100 * np.max(predictions_array),
+                                         class_names[true_label]),
+               color=color)
+
+
+def plot_value_array(i, predictions_array, true_label):
+    predictions_array, true_label = predictions_array[i], true_label[i]
+    plt.grid(False)
+    plt.xticks([])
+    plt.yticks([])
+    thisplot = plt.bar(range(10), predictions_array, color="#777777")
+    plt.ylim([0, 1])
+    predicted_label = np.argmax(predictions_array)
+
+    thisplot[predicted_label].set_color('red')
+    thisplot[true_label].set_color('blue')
+
+
 if __name__ == '__main__':
     tf.logging.set_verbosity(tf.logging.ERROR)
+    config = tf.ConfigProto()
+    config.gpu_options.allow_growth = True
+    config.allow_soft_placement = True
+    config.gpu_options.per_process_gpu_memory_fraction = 0.8
 
     import math
     import matplotlib.pyplot as plt
@@ -51,8 +88,9 @@ if __name__ == '__main__':
     plt.show()
 
     model = tf.keras.Sequential([
-        tf.keras.layers.Flatten(input_shape=(28,28,1)),
-        tf.keras.layers.Dense(128, activation=tf.nn.relu),
+        tf.keras.layers.Flatten(input_shape=(28, 28, 1)),
+        tf.keras.layers.Dense(10, activation=tf.nn.softmax),
+        tf.keras.layers.Dense(512, activation=tf.nn.relu),
         tf.keras.layers.Dense(10, activation=tf.nn.softmax)
     ])
 
@@ -62,12 +100,27 @@ if __name__ == '__main__':
     train_dataset = train_dataset.repeat().shuffle(num_train_examples).batch(BATCH_SIZE)
     test_dataset = test_dataset.batch(BATCH_SIZE)
 
-    model.fit(train_dataset, epochs=5, steps_per_epoch=math.ceil(num_train_examples/BATCH_SIZE))
+    model.fit(train_dataset, epochs=5, steps_per_epoch=math.ceil(num_train_examples / BATCH_SIZE))
 
-    test_loss, test_accuracy = model.evaluate(test_dataset, steps=math.ceil(num_test_examples/32))
+    test_loss, test_accuracy = model.evaluate(test_dataset, steps=math.ceil(num_test_examples / 32))
     print('测试精确度：', test_accuracy)
 
     for test_images, test_labels in test_dataset.take(1):
         test_images = test_images.numpy()
         test_labels = test_labels.numpy()
-        pridictions = model.predict(test_images)
+        predictions = model.predict(test_images)
+
+    predictions.shape
+    print(predictions[0])
+    print(np.argmax(predictions[0]))
+
+    num_rows = 5
+    num_cols = 3
+    num_images = num_rows * num_cols
+    plt.figure(figsize=(2 * 2 * num_cols, 2 * num_rows))
+    for i in range(num_images):
+        plt.subplot(num_rows, 2 * num_cols, 2 * i + 1)
+        plot_image(i, predictions, test_labels, test_images)
+        plt.subplot(num_rows, 2 * num_cols, 2 * i + 2)
+        plot_value_array(i, predictions, test_labels)
+    plt.show()
